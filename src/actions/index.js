14bfb20bb2cb1970b93eda4506de60e5e3f3ac4b
DarkMass27 <jonathanscialpi@gmail.com>
2018-10-18 07:25:32 -0400

    redux-promise in practice

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API_KEY = '4b3aa5db3bd325522bff4902ad09015c';
-const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
